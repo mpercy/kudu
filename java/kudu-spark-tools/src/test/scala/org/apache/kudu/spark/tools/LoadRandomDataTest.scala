2cfab148baf5f570edefba523def8d1b7fe642a8
Mike Percy <mpercy@apache.org>
2018-07-23 18:55:54 -0700

    WIP: Create parallelized loader Spark job
    This patch adds a new tool called LoadRandomData that will load random
    data into a table in a parallelized manner. Perhaps the tool should be
    named something else so it would be straightforward to add a sequential
    inserter that supports a range-partitioned table.
    
    Marked WIP because it could use more testing.
    
    Change-Id: I4434c9f5d709154037386b4c7be94045df162267

---
+++ java/kudu-spark-tools/src/test/scala/org/apache/kudu/spark/tools/LoadRandomDataTest.scala
@@ -0,0 +1,54 @@
+package org.apache.kudu.spark.tools
+
+import com.google.common.collect.ImmutableList
+import org.apache.kudu.ColumnSchema.ColumnSchemaBuilder
+import org.apache.kudu.client.CreateTableOptions
+import org.apache.kudu.Schema
+import org.apache.kudu.Type
+import org.apache.kudu.spark.kudu.KuduTestSuite
+import org.junit.Test
+import org.junit.Assert.assertEquals
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import scala.collection.JavaConverters._
+
+@RunWith(classOf[JUnitRunner])
+class LoadRandomDataTest extends KuduTestSuite {
+
+  private val TABLE_NAME: String = "LoadRandomDataTest"
+
+  @Test
+  def testSparkImportExport() {
+
+    val schema: Schema = {
+      val columns = ImmutableList.of(
+        new ColumnSchemaBuilder("key", Type.INT64).key(true).build(),
+        new ColumnSchemaBuilder("column1_i", Type.INT64).build(),
+        new ColumnSchemaBuilder("column2_i", Type.INT64).build(),
+        new ColumnSchemaBuilder("column3_s", Type.STRING).build(),
+        new ColumnSchemaBuilder("column4_s", Type.STRING).build()
+      )
+      new Schema(columns)
+    }
+    val tableOptions = new CreateTableOptions()
+      .setRangePartitionColumns(List("key").asJava)
+      .setNumReplicas(1)
+    kuduClient.createTable(TABLE_NAME, schema, tableOptions)
+
+    val NumRows = 100
+    LoadRandomData.testMain(
+      Array(
+        s"--num-rows=$NumRows",
+        "--num-tasks=10",
+        "--string-field-len=128",
+        TABLE_NAME,
+        miniCluster.getMasterAddresses
+      ),
+      ss
+    )
+    val rdd = kuduContext.kuduRDD(ss.sparkContext, TABLE_NAME, List("key"))
+    assertEquals(NumRows, rdd.collect.length)
+  }
+
+}
