07196f4013d537b884b114e2b09a07eba1b1c1b6
Mike Percy <mpercy@apache.org>
2018-08-03 20:51:14 -0700

    common: Add WARN_UNUSED_RESULT to encoding functions
    We should get warnings if we don't check the result on functions that
    allocate from an Arena.
    
    Change-Id: I60ab19fa3b9092fbf489b4e9a74cbbf258df2ca1

--- src/kudu/common/encoded_key.h
+++ src/kudu/common/encoded_key.h
@@ -55,12 +55,12 @@ class EncodedKey {
   static Status DecodeEncodedString(const Schema& schema,
                                     Arena* arena,
                                     const Slice& encoded,
-                                    gscoped_ptr<EncodedKey> *result);
+                                    gscoped_ptr<EncodedKey> *result) WARN_UNUSED_RESULT;
 
   // Given an EncodedKey, increment it to the next lexicographically greater EncodedKey.
   static Status IncrementEncodedKey(const Schema& tablet_schema,
                                     gscoped_ptr<EncodedKey>* key,
-                                    Arena* arena, int num_columns = 0);
+                                    Arena* arena, int num_columns = 0) WARN_UNUSED_RESULT;
 
   const Slice &encoded_key() const { return encoded_key_; }
 
